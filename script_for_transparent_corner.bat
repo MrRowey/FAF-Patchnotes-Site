ae1da4406416f58e3280b7b7dbc0176f8649408d
MrRowey <row.josh02@gmail.com>
2023-05-30 09:43:05 +0100

    remove Unrelated files

--- script_for_transparent_corner.bat
+++
@@ -1,9 +0,0 @@
-rem Put and run this in same folder with pics you want to modify
-rem ImageMagick is required, http://www.imagemagick.org/script/binary-releases.php#windows
-rem script could be improved with just outlining the corners and modifying that area straight to transparent
-rem Modified pics will be in transp folder
-setlocal enabledelayedexpansion
-
-for %%i in (*.png) do (
-	convert %%i -transparent white transp\%%i
-)
