7809d0d036d81345715a208bce0f75851013deac
Josh <row.josh02@gmail.com>
2024-06-30 14:39:31 +0100

    test

--- scripts/populateOldGamePatches.js
+++ scripts/populateOldGamePatches.js
@@ -1,41 +1,41 @@
 async function populate() {
-    const requestURL = '../assets/data/oldgamepatches.json';
+  const requestURL = "../assets/data/oldgamepatches.json";
 
-    try {
-        const response = await fetch(requestURL);
-        if (!response.ok) {
-            throw new Error(`Network response was not ok: ${response.statusText}`);
-        }
-        const patches = await response.json();
-        renderPatchList(patches.game, ".gameList");
-    } catch (error) {
-        console.error('There has been a problem with your fetch operation:', error);
+  try {
+    const response = await fetch(requestURL);
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.statusText}`);
     }
+    const patches = await response.json();
+    renderPatchList(patches.game, ".gameList");
+  } catch (error) {
+    console.error("There has been a problem with your fetch operation:", error);
+  }
 }
 
 function renderPatchList(patchList, containerSelector) {
-    const container = document.querySelector(containerSelector);
-    if (!container) {
-        console.error(`Container with selector ${containerSelector} not found`);
-        return;
-    }
+  const container = document.querySelector(containerSelector);
+  if (!container) {
+    console.error(`Container with selector ${containerSelector} not found`);
+    return;
+  }
 
-    const list = document.createElement("ul");
+  const list = document.createElement("ul");
 
-    patchList.forEach(({patch, link, date}) => {
-        const listItem = document.createElement("li");
+  patchList.forEach(({ patch, link, date }) => {
+    const listItem = document.createElement("li");
 
-        const linkElement  = document.createElement("a");
-        linkElement .textContent = patch;
-        linkElement .href = link;
+    const linkElement = document.createElement("a");
+    linkElement.textContent = patch;
+    linkElement.href = link;
 
-        const dateElement  = document.createElement("span");
-        dateElement .textContent = date;
+    const dateElement = document.createElement("span");
+    dateElement.textContent = date;
 
-        listItem.append(linkElement , dateElement );
-        list.appendChild(listItem);
-    });
+    listItem.append(linkElement, dateElement);
+    list.appendChild(listItem);
+  });
 
-    container.appendChild(list);
+  container.appendChild(list);
 }
 populate();
