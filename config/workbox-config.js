fdf12f3822b3eb577c6bdad50060848b14f45acb
Josh <row.josh02@gmail.com>
2025-02-28 17:50:44 +0000

    Config file

--- config/workbox-config.js
+++ config/workbox-config.js
@@ -1,4 +1,7 @@
-export const globDirectory = "src/_site";
-export const globPatterns = ["**/*.{html,css,svg,json,xml,js}"];
-export const swDest = "src/_site/service-worker.js";
-export const ignoreURLParametersMatching = [/^utm_/, /^fbclid$/];
+module.exports = {
+    globDirectory: "src/_site",
+    globPatterns: ["**/*.{html,css,svg,json,xml,js}"],
+    swDest: "src/_site/service-worker.js",
+    ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+  };
+  
